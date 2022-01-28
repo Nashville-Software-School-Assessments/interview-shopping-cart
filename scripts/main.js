9ee1f0a55d503444a7253dd168eee57e4838cc67
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2022-01-28 10:15:07 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
 /*
+    Learning objectives assessed in this exercise:
+        1. Functions, arguments, parameters, return values
+        2. Array iteration
+        3. Debugging with Dev Tools
+        4. Variable scope
+        5. Algorithmic thinking
+        
     You need to figure out how to make the message display the 
-    total cost of items added to the shopping cart.
+    total cost of items added to the shopping cart. We strongly
+    suggest that you read the current code, understand what each
+    line does, and write comments before you start fixing any
+    bugs, or writing new code.
 */
 
 const shoppingCart = []
@@ -25,6 +35,7 @@ const plushToy = { id: 1, price: 14.78, name: "Beanie Baby" }
 const kite = { id: 2, price: 8.49, name: "Dragon Kite" }
 const watch = { id: 3, price: 102.32, name: "Seiko Kinetic Wristwatch" }
 
+// This might be a good place to write code to add items to the shopping cart
 
 
 /*
